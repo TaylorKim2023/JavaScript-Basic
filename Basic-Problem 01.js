bb1d19fa0273eee1a5ffee3369f1b017e9a88693
TaylorKim2023 <anworld2023@gmail.com>
2023-01-11 21:49:39 +0900

    자바스크립트 기초 알고리즘 문제 풀이 01일차

---
+++ Basic-Problem 01.js
@@ -0,0 +1,11 @@
+
+// 1부터 N까지의 더하기 문제. 
+
+function solution(n) {
+    let output=0; 
+    for(let i=1;i<=n;i++) {
+        output=output+i;  
+        }
+        return output; 
+    }
+console.log(solution(10)); 
